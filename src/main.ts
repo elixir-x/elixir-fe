08caf0fe8c29a300b672d6b76829a7f5cbef4089
dingo <realdinozoid@gmail.com>
2022-07-06 12:56:14 -0500

    fixed build + staging deployment

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { createPersistedState } from 'pinia-plugin-persistedstate'
 import App from './App.vue';
 import router from './router';
 import '../index.css';
-import dotenv from 'dotenv';
-
-// dotenv.config();
 
 const pinia = createPinia();
 pinia.use(createPersistedState());
